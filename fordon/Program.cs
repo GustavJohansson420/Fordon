18c8a3a7f18c7abce793d2a0caad55df40550ea3
GustavJohansson420 <89460949+GustavJohansson420@users.noreply.github.com>
2022-01-21 15:49:38 +0100

    Add files via upload

---
+++ fordon/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fordon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fordon f = new Fordon();
+            f.Märke = "Volvo";
+            f.Modell = "V70";
+            f.Årsmodell = 2016;
+
+            Console.WriteLine(f.Märke);
+            Console.WriteLine(f.Modell);
+            Console.WriteLine(f.Årsmodell);
+        }
+    }
+}
---
+++ fordon/fordon.cs
@@ -0,0 +1,24 @@
+class Fordon
+{
+    private string märke;
+    private string modell;
+    private int årsmodell;
+
+    public string Märke
+    {
+        get { return märke; }
+        set { märke = value; }
+    }
+
+    public string Modell
+    {
+        get { return modell; }
+        set { modell = value; }
+    }
+    
+    public int Årsmodell
+    {
+        get { return årsmodell; }
+        set { årsmodell = value; }
+    }
+}
